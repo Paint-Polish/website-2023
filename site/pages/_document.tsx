b8596de1341b2213c1ff7e9bf0311e0f3cae813d
Ed Oswald Go <ed@lexagle.com>
2023-04-19 16:07:38 +0800

    Add support for Meta Pixel

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -19,6 +19,20 @@ class MyDocument extends Document {
               gtag('config', 'G-DKFBDRFMCQ');
             `}
           </Script>
+          <Script id="fb-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '548020040776155');
+              fbq('track', 'PageView');
+            `}
+          </Script>
         </Head>
         <body className="loading">
           <Main />
