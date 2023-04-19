d42bb233425f6afcd958af05efa63f04e300ed71
Ed Oswald Go <ed@lexagle.com>
2023-04-19 16:07:13 +0800

    Add support for Google Analytics

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -1,10 +1,25 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-DKFBDRFMCQ"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-DKFBDRFMCQ');
+            `}
+          </Script>
+        </Head>
         <body className="loading">
           <Main />
           <NextScript />
